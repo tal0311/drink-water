5a3a93785b9362110e4f4c03319cb7057dceec66
tal0311 <tal.amit0311@gmail.com>
2021-09-21 17:53:35 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.querySelector('#liters')
+const precentage = document.querySelector('#precentage')
+const remained = document.querySelector('#remaind')
+console.log(precentage)
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener('click', () => highlightCups(index))
+})
+
+function highlightCups(index) {
+  smallCups.forEach((cup, index2) => {
+    if (index2 === index) {
+      cup.classList.toggle('full')
+    }
+  })
+
+  updateBigCup()
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  const totalCups = smallCups.length
+  console.log(fullCups, totalCups)
+
+  precentage.style.height = `${(fullCups / totalCups) * 330}px`
+  precentage.innerText = `${(fullCups / totalCups) * 100}%`
+  liters.innerText = `${100 - (fullCups / totalCups) * 100}% `
+
+  if (fullCups === totalCups) {
+    remained.style.visability = 'hidden'
+    remained.style.height = 0
+  }
+}
